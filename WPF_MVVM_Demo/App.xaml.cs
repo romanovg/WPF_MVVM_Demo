b2cbaa244a0e468f447803a9f477e11b3814ef7f
Gleb Romanov <gleb.romanov@outlook.com>
2019-12-15 22:38:06 -0700

    View Model First

--- WPF_MVVM_Demo/App.xaml.cs
+++ WPF_MVVM_Demo/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WPF_MVVM_Demo.ViewModels;
 
 namespace WPF_MVVM_Demo
 {
@@ -13,5 +14,11 @@ namespace WPF_MVVM_Demo
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            window.Content = new PersonViewModel();
+        }
     }
 }
