c8495c11b640f854a9113b80bfebc30e480733f9
Gleb Romanov <gleb.romanov@outlook.com>
2019-12-15 22:09:02 -0700

    Data Binding - View first

---
+++ WPF_MVVM_Demo/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_Demo.Models
+{
+    public class Person
+    {
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ WPF_MVVM_Demo/ViewModels/PersonViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MVVM_Demo.Models;
+
+namespace WPF_MVVM_Demo.ViewModels
+{
+    public class PersonViewModel
+    {
+        private Person _person;
+
+        public string FullName
+        {
+            get { return string.Concat(_person.LastName, ", ", _person.FirstName); }
+        }
+
+        public PersonViewModel()
+        {
+            _person = new Person("John", "Doe");
+        }
+    }
+}
