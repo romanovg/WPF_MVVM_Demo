d998927ce71dcb9f2a5efb8f84a97de58f5496d5
Gleb Romanov <gleb.romanov@outlook.com>
2019-12-18 00:30:54 -0700

    Events And Commands

--- WPF_MVVM_Demo/App.xaml.cs
+++ WPF_MVVM_Demo/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,11 +16,21 @@ namespace WPF_MVVM_Demo
     /// </summary>
     public partial class App : Application
     {
+        private List<Person> _people = new List<Person>() { new Person("John", "Doe") };
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             MainWindow window = new MainWindow();
             window.Show();
-            window.Content = new PersonViewModel(new Person("John", "Doe"));
+            window.Content = new PersonListViewModel(_people);
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            foreach (Person person in _people)
+            {
+                Debug.WriteLine($"{person.LastName}, {person.FirstName}");
+            }
         }
     }
 }
---
+++ WPF_MVVM_Demo/ViewModels/PersonListViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_MVVM_Demo.Helpers;
+using WPF_MVVM_Demo.Models;
+
+namespace WPF_MVVM_Demo.ViewModels
+{
+    public class PersonListViewModel : BaseViewModel
+    {
+        private List<Person> _people;
+
+        private string _newPersonFirstName = string.Empty;
+        public string NewPersonFirstName
+        {
+            get { return _newPersonFirstName; }
+            set { _newPersonFirstName = value; RaisePropertyChanged(nameof(NewPersonFirstName)); RaisePropertyChanged(nameof(CanAddPerson)); }
+        }
+
+        private string _newPersonLastName = string.Empty;
+        public string NewPersonLastName
+        {
+            get { return _newPersonLastName; }
+            set { _newPersonLastName = value; RaisePropertyChanged(nameof(NewPersonLastName)); RaisePropertyChanged(nameof(CanAddPerson)); }
+        }
+
+        public bool CanAddPerson
+        {
+            get { return NewPersonFirstName.Length > 0 & NewPersonLastName.Length > 0; }
+        }
+
+        public ICommand AddNewPersonCommand { get; }
+
+        public ReadOnlyCollection<PersonViewModel> People
+        {
+            get
+            {
+                List<PersonViewModel> temp = new List<PersonViewModel>();
+                foreach (Person person in _people)
+                {
+                    temp.Add(new PersonViewModel(person));
+                }
+                return new ReadOnlyCollection<PersonViewModel>(temp);
+            }
+        }
+
+        public PersonListViewModel(List<Person> people)
+        {
+            _people = people;
+            AddNewPersonCommand = new RelayCommand(_ => AddPerson(), _ => CanAddPerson);
+        }
+
+        private void AddPerson()
+        {
+            _people.Add(new Person(NewPersonFirstName, NewPersonLastName));
+            RaisePropertyChanged(nameof(People));
+            NewPersonFirstName = string.Empty;
+            NewPersonLastName = string.Empty;
+        }
+    }
+}
